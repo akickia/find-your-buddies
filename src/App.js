936b1170929a3836f4350da5cf1120cff63393f4
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2022-05-25 16:20:42 +0200

    created Welcome, Footer and Button

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
+import Footer from "./components/Footer";
+import Welcome from "./components/Welcome";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+
+      <Welcome />
+      <Footer />
     </div>
   );
 }
