2c7d0bfd2a78d12cd0ccc32ba3fda73248c42579
car0linb <98027632+car0linb@users.noreply.github.com>
2022-05-25 23:11:06 +0200

    beginning of log in
    Added todo and started adding form with input fields. Added functions.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,14 @@
+import {useState} from "react";
+
 const Login = () =>{
     return(
-        <div></div>
+        <div>
+            <label>Your badass name</label>
+            <input autofocus type = "text" name ="name" id ="name"></input>
+            <label>Password</label>
+            <input type = "password" name ="password" id="password"></input>
+            <button>Log me right in!</button>
+        </div>
     );
 };
 
