4f868d94d77cfd6c36bd44e02609d9f1d857a88d
Åsa Hildebrand <98025185+HildebrandAsa@users.noreply.github.com>
2022-05-30 08:29:39 +0200

    Added some logic to sign up an added button component

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,25 @@
+import Button from "./Button"
+
 const Signup = () => {
   return (
-    <div>
-      Login
+    <div className = "container">
+      <h2>Sign Up</h2>
+      <h3>so that you can start finding your buddies!</h3>
+      <form className = "form">
+        <label htmlFor="userName" >What's your name?</label>
+          <input
+          type = "text"
+          name = "userName"
+          id = "userName"
+          />
+          <label htmlFor="password" >What password would you like?</label>
+          <input
+          type = "password"
+          name = "password"
+          id = "password"
+          />
+          <Button link="#">Sign up and log in</Button>
+      </form>
     </div>
   );
 };
