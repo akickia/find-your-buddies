2bd4647c52b20833700df26f6e2c43f20877313b
car0linb <98027632+car0linb@users.noreply.github.com>
2022-06-03 10:13:03 +0200

    browser router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Signup from "./components/Signup";
+import Header from "./components/Header";
+import ShowChoices from "./components/ShowChoices";
+import Footer from "./components/Footer";
+import Welcome from "./components/Welcome";
+import Adventure from "./components/Adventure";
+
 import Login from "./components/Login";
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
 
-      <Login />
+      
+      <BrowserRouter>
+        <Header />
+        <div className ="container">
+          <Routes>
+            <Route path = "/" element = {<Welcome />}/>
+            <Route path = "/showchoices" exact element = {<ShowChoices />}/>
+            <Route path = "/adventure" exact element = {<Adventure />}/>
+            <Route path = "/signup" exact element = {<Signup />}/>
+            <Route path = "/login" exact element = {<Login />}/>
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+     
 
     </div>
   );
