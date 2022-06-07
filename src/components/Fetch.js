19510f74dc69c945c9927949e05779f2b29a5a62
Kicki Lindstrand <kicki.lindstrand@gmail.com>
2022-06-07 09:53:16 +0200

    added img för buddies

---
+++ src/components/Fetch.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react"
+
+const pokemon = {
+  id: null,
+  name: null,
+}
+
+
+const Fetch = () => {
+  const [pokemon, setPokemon] = useState([]);
+ 
+
+
+  const getPokemon = async () => {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/5/`);
+      const data = await response.json();
+      console.log(data)
+      setPokemon(data)
+    }
+    
+    useEffect (() => {
+    getPokemon();
+  }, []);
+  
+  
+  return (
+    <>
+    
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png" alt="" />
+    
+    </>
+  )
+}
+
+export default Fetch
--- src/components/adventure/Mary.js
+++ src/components/adventure/Mary.js
@@ -8,6 +8,7 @@ const Mary = () => {
     <div className="events">
 
           <h3>You found Mary!</h3>
+          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png" alt="" />
           <p>Thank you for finding me! I was really lost!</p>
           <p>What do you want to do now?</p>
           <Link to="/choice1"><Button name= "Click to find more friends"></Button></Link>
