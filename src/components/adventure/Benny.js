b8c164d59d640b7ef2ca29ec1de114e4e3ce3783
car0linb <98027632+car0linb@users.noreply.github.com>
2022-06-07 11:26:08 +0200

    Update Benny.js

--- src/components/adventure/Benny.js
+++ src/components/adventure/Benny.js
@@ -9,6 +9,7 @@ const Benny = () => {
           <h3>You found Benny!</h3>
           <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/40.png" alt="" />
           <p>Thank you for finding me! I was so lonely!</p>
+          <Button name= "Collect me"></Button>
           <Link to="/choice2"><Button name= "Click to continue"></Button></Link>
     </div>
     
